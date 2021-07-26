3897ef13ccc925fda1096aa238a1db141db12f46
Jim12345 <78886234+Ivernys-clone@users.noreply.github.com>
2021-07-27 09:04:56 +1200

    Update quiz_v2.py

--- assembled quiz/quiz_v2.py
+++ assembled quiz/quiz_v2.py
@@ -2,31 +2,33 @@
 # This program is to challenge those who want to test their own skills in Math
 #imports random module
 import random
-print("**************************\nWelcome to Jims maths quiz\n**************************")
+#Sets score to 0
+score=0
+#Initialises dictionary
+questions = {}
 
+def hello():
+    print("**************************")
+    print("Welcome to Jims maths quiz")
+    print("**************************\n")
+    
 #using funtions to ask for name and .isalpha to catch errors
-def user_details(name): #using def to be able to call the function many times easily
-    print("Hello", name,) # greeting user
-while True: #starting a loop
-    name = input("Please enter your first name : ").capitalize() #asking name.
-    if name.isalpha(): # .isalpha to trap errors
+def user_details(): #using def to be able to call the function many times easily
+    global name
+    while True: #starting a loop
+        name = input("Please enter your first name : ").capitalize() #asking name.
+        if name.isalpha(): # .isalpha to trap errors
             break # breaking the loop
-
-    else:
-        print("=========================")
-        print("Please enter only letters")
-        print("=========================")
-
-#Outputs the function
-user_details(name)
+        else:
+            print("\n=========================")
+            print("Please enter only letters")
+            print("=========================\n")
 
 #Asking if they want to know the quiz rules
 def instructions():
     while True:
-   
-        inst = input("Would you like to read the instructions?\na)Yes\nb)no\nEnter here: ").upper()
-   
-        if inst == "YES" or inst == "Y" or inst == "A":
+        inst = input("\nWould you like to read the instructions?\na)Yes\nb)No\nEnter here: ").lower()
+        if inst == "yes" or inst == "y" or inst == "a":
             print("""The instructions are simple you will be given a random generated maths quiz
 Read the question and answer it.
 Example: 1+1:  <--Put your answer here
@@ -35,46 +37,45 @@ You can choose the number of rounds you would like to play.
 You will be prompted to enter a number of questions you want to play.
 """)
             break
-        if inst == "NO" or inst == "N" or inst == "" or inst == "B":
+        if inst == "no" or inst == "n" or inst == "" or inst == "b":
             break
         else:
-            print("=============================\nPlease enter the options only\n=============================")
-      
-#Outputs the function   
-instructions()
-#Asks user if they want to quit the Quiz
+            print("\n=============================\nPlease enter the options only\n=============================")  
 
+#Asks user if they want to quit the Quiz
 def status(): 
   while True: #Starting a loop
-    shutdown = input("\nDo you wish to play this quiz?  \na)Yes\nb)no\nEnter here:").upper()
-    if shutdown == "YES" or shutdown == "Y" or shutdown == "A" or shutdown == "":
+    shutdown = input("\nDo you wish to play this quiz?\nEnter (yes or enter) to continue or any key to continue : ").lower()
+    if shutdown == "yes" or shutdown == "y" or shutdown == "a" or shutdown == "":
       break
     else:
-        print("\nThank you please play the quiz again")
+        print("\nThank you please play the quiz again.")
         exit()
-status()
-        
+      
 def rounds():
-            global num, total
-            while True:
-                        try:
-                         
-                                    num = int(input("\nPlease enter the number of questions you would like to answer between 1-10: "))
-                                    if 0<num<=10: #not allowing less than 1 and more than 10
-                                                break
-                                    else:
-                                                print("\n==============================\nPlease enter numbers 1-10 only\n==============================")
-                        except:
-                                    print("\n=========================\nPlease enter numbers only\n=========================")
+    global num, total
+    while True:
+        try:
+            num = int(input("\nPlease enter the number of questions you would like to answer between 1-10: "))
+            if 0<num<=10:
+                break
+            else:
+                print("\n==============================\nPlease enter numbers 1-10 only\n============================")
+        except: 
+                                print("\n=========================\nPlease enter numbers only\n=========================")
+    total = num
 
-            total = num
+
+#Outputs functions
+hello()
+user_details()
+instructions()
+status()
+print("\nNote: If you ever want to leave the quiz enter xxx.\n")
 rounds()
 
-print("\nNote: If you ever want to leave the quiz enter xxx\n")
-#Sets score to 0
-score=0
-#Initialises dictionary
-questions = {}
+
+
 
 #Maths questions are randomly generated with number betweem 0 and 10 
 #(+, -, *) operators are chosen at random.
@@ -92,19 +93,23 @@ for i in range(num):
       questions.update({question:answer})
     else:
       continue
-   
-
 #Checks if the answer is right or wrong
 for q in questions.keys():
-    user_answer=input(q)
-    if questions.get(q)==user_answer:
-        print("\n*****************\nYou got it right!\n*****************\n")
-        
-        score+=1
-    else:
-        print("\n#################\nYou got it wrong!\n#################\n")
+    while True:
+        user_answer=input(q)
+        if user_answer.lstrip('-').isdigit():
+            if questions.get(q)==user_answer:
+                print("\n*****************\nYou got it right!\n*****************\n")
+                score+=1
+                break
+            else:
+                print("\n*****************\nYou got it wrong!")
+                print("The answer is: "+questions.get(q)+"\n*****************")
+                break
+        else:
+            print("\n=======================\nPlease enter an integer\n=======================")
 #Outputs the users score and percentage
-print("\n**************************************************\n")
+print("\n*******************************************************************************************\n")
 print("Thank you for using Jim's maths quiz")
 #str converts int to string
 print("Your final score is "+str(score)+" out of "+str(total))
